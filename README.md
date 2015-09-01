b75d87d111e46848aaa614be3b1bfb9d4d8fb4f7
worace <horace.d.williams@gmail.com>
2015-09-01 15:08:15 -0600

    putting together basic indexing task

--- README.md
+++ README.md
@@ -17,3 +17,29 @@ your option) any later version.
 ### Daemonization
 
 Based on [this article](http://www.rkn.io/2014/02/06/clojure-cookbook-daemons/), using the Apache Commons Daemons library.
+
+### Random Notes
+
+
+;; message fields
+;; {"type":"message","channel":"C02Q114D3","user":"U02QCFVPS","text":"haha ^","ts":"1440708033.000049","team":"T029P2S9M"}
+;; index -- text, user, channel
+;; message id -- channel + ts combo -- ts is guaranteed unique by channel
+
+
+
+;; expected slack event types:
+#_#{"message" ;; -- some messages also have special subtypes
+  "user_typing"
+  "im_created"
+  "star_removed"
+  "user_change"
+  "reaction_added"
+  "file_change"
+  "pong"
+  "reaction_removed"
+  "star_added"
+  "emoji_changed"
+  "file_public"
+  "presence_change"
+  "file_shared"}
--- src/slack_slurper/indexing.clj
+++ src/slack_slurper/indexing.clj
@@ -2,43 +2,59 @@
   (:require [cheshire.core :as json]
             [clojurewerkz.elastisch.rest :as esr]
             [clojurewerkz.elastisch.rest.index :as esi]
+            [clojurewerkz.elastisch.query         :as q]
             [clojurewerkz.elastisch.rest.document :as esd]))
 
 
 (def es-conn (esr/connect "http://127.0.0.1:9200"))
+(def index-name "slack_slurper")
+(def mappings
+  {"messages" {:properties {:user    {:type "string" :store "yes"}
+                            :channel {:type "string" :store "yes"}
+                            :text    {:type "string" :store "yes" :analyzer "standard"}
+                            :subtype {:type "string" :store "yes"}
+                            :ts      {:type "string" :store "yes"}}}})
 
-(defn create-index [conn name]
-  (esi/create conn name))
+(def create-index! (partial esi/create es-conn index-name :mappings mappings))
+(def delete-index! (partial esi/delete es-conn index-name))
+
+(defn log-files
+  ([] (log-files "./logs"))
+  ([dir] (filter (fn [f] (and (.isFile f)
+                              (.endsWith (.getName f) ".log")))
+                 (file-seq (clojure.java.io/file dir)))))
 
 (defn extract-message [string]
   (last (clojure.string/split string #"- ")))
 
-;; expected message types:
-#_#{"message"
-  "user_typing"
-  "im_created"
-  "star_removed"
-  "user_change"
-  "reaction_added"
-  "file_change"
-  "pong"
-  "reaction_removed"
-  "star_added"
-  "emoji_changed"
-  "file_public"
-  "presence_change"
-  "file_shared"}
+(defn log-file->messages [f]
+  (with-open [r (clojure.java.io/reader f)]
+    (doall (->> (line-seq r)
+                (map extract-message)
+                (filter (fn [m] (.contains m "{\"type")))
+                (map json/parse-string)
+                (filter (fn [m] (= "message" (m "type"))))) )))
 
-;; subtypes -- some messages have special subtypes
-;; good - file_share
-;; ignore - channel joined
+(defn messages [log-files]
+  (mapcat log-file->messages log-files))
 
-(defn log-files
-  ([] (log-files "./logs"))
-  ([dir] (filter (fn [f] (.isFile f))
-                 (file-seq (clojure.java.io/file dir)))))
+(defn message-id [m]
+  (str (m "channel") "-" (m "ts")))
 
-#_(->> logs
-     (map extract-message)
-     (filter (fn [m] (.contains m "{")))
-     (map json/parse-string))
+(defn index-message! [m]
+  (esd/create es-conn
+              index-name
+              "message"
+              (dissoc m "type" "team")
+              :id (message-id m)))
+
+(defn index-messages! [messages]
+  (doseq [m messages]
+    (index-message! m)))
+
+(defn rebuild-index!
+  "Destroys and rebuilds the index using existing log files as input source."
+  []
+  (delete-index!)
+  (create-index!)
+  (index-messages! (messages (log-files))))
