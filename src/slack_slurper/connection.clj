11d2455677c44e6e6abe263705951a8bc4b59fa4
worace <horace.d.williams@gmail.com>
2015-09-19 11:27:26 -0600

    add new connection manager ns

---
+++ src/slack_slurper/connection.clj
@@ -0,0 +1,12 @@
+(ns slack-slurper.connection
+  (:require [aleph.http :as http]
+            [clj-slack-client.web :as slack-api]))
+
+(def api-token (System/getenv "SLACK_SLURPER_TOKEN"))
+
+(defn get-fresh-ws-url []
+  (:url (slack-api/rtm-start api-token)))
+
+(defn ws-stream
+  ([] (ws-stream (get-fresh-ws-url)))
+  ([url] @(http/websocket-client url)))
